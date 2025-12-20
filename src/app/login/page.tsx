ff932dc368f0c65475539b5376ad7e6b4778246c
Tribed Mahato <tribedmahato61@gmail.com>
2025-12-20 04:34:10 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -66,7 +66,7 @@ export default function LoginPage() {
       }
       
       router.push("/");
-    } catch (error: any) => {
+    } catch (error: any) {
       toast({
         variant: "destructive",
         title: "Authentication Failed",
