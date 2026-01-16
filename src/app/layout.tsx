978b28116a0f4e8302cfa50edd41e14aa31856fc
Tribed Mahato <tribedmahato61@gmail.com>
2026-01-16 18:07:41 +0000

    I am using a Next.js website hosted on Vercel. Please completely remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from '@/firebase/client-provider';
@@ -21,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full dark">
       <head>
-        <meta name="monetag" content="bd3b3e022f9931862345738513c7161c" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
@@ -31,12 +29,6 @@ export default function RootLayout({
           {children}
         </FirebaseClientProvider>
         <Toaster />
-        <Script 
-          src="https://quge5.com/88/tag.min.js"
-          strategy="afterInteractive"
-          data-zone="202435" 
-          data-cfasync="false"
-        />
       </body>
     </html>
   );
