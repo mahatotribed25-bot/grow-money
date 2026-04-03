87bda34eb0b98db224efddd78f66fcd71d9ce841
Tribed Mahato <tribedmahato61@gmail.com>
2026-04-03 18:53:07 +0000

    <script async="async" data-cfasync="false" src="https://pl28494441.profi

--- src/components/ads/AdsterraNativeBanner.tsx
+++ src/components/ads/AdsterraNativeBanner.tsx
@@ -4,12 +4,15 @@ import Script from 'next/script';
 
 export function AdsterraNativeBanner() {
   return (
-    <Script
-      id="adsterra-ad"
-      async
-      data-cfasync="false"
-      src="https://pl28506232.profitablecpmratenetwork.com/e5/a4/32/e5a432744ffdcb052d30bec5ba0f793e.js"
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        id="adsterra-ad-invoker"
+        async={true}
+        data-cfasync="false"
+        src="https://pl28494441.profitablecpmratenetwork.com/10d601e6e5e0c81bf640524d6092e5f3/invoke.js"
+        strategy="afterInteractive"
+      />
+      <div id="container-10d601e6e5e0c81bf640524d6092e5f3" />
+    </>
   );
 }
