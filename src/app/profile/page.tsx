4ffde6c05a53ca293cb5e8ff2b39b20bfd44f243
Tribed Mahato <tribedmahato61@gmail.com>
2026-09-13 08:24:28 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -51,7 +51,7 @@ import {
   DialogFooter,
   DialogClose,
 } from '@/components/ui/dialog';
-import { ScrollArea } from '../ui/scroll-area';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Separator } from '@/components/ui/separator';
 import { Progress } from '@/components/ui/progress';
